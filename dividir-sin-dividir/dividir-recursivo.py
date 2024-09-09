7a9ae9e71f3e9f63c467c797c17db4b8121dbde4
cesarSPC <149351724+cesarSPC@users.noreply.github.com>
2024-09-08 23:04:40 -0500

    Update dividir-recursivo.py

--- dividir-sin-dividir/dividir-recursivo.py
+++ dividir-sin-dividir/dividir-recursivo.py
@@ -1,10 +1,11 @@
-def dividir_recursivo(dividendo, divisor, iteraciones = 0):
+def dividir_recursivo(dividendo, divisor):
     resta = dividendo-divisor
+    if divisor == 0:
+        return "Inválido"
     if resta < 0:
-        return iteraciones
-    if resta == 0:
-        return iteraciones + 1
-    return dividir_recursivo(resta, divisor, iteraciones + 1)
+        return 0
+    return 1 + dividir_recursivo(resta, divisor) # Cuenta cuantas veces se realiza la división
     
 print(dividir_recursivo(40, 9))
 print(dividir_recursivo(40, 5))
+print(dividir_recursivo(0,3))
